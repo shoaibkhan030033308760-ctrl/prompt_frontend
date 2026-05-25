6f354aee8739b5c62c80bc58f55c3fe3b5816d86
shoaib <shoaibkhan030033308760@gmail.com>
2026-05-25 14:34:07 +0500

    fix

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11054474
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+
+// yar ya samaj nahi ati wo ya ka ma kya karo and ya bhi sawal ha mara wo ya ka ma kya kr sakta ho 
